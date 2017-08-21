fad8bb16fd904c18de8b613d81a767fab99ee72a
aivven <aivven@gmail.com>
2017-08-21 21:15:21 +0300

    Личный проект: милый DOM

--- js/pictures.js
+++ js/pictures.js
@@ -7,9 +7,9 @@ var template = document.querySelector('#picture-template').content.querySelector
 function createUsers() {
   function getRandomInt(min, max) {
     var rand = min - 0.5 + Math.random() * (max - min + 1);
-    random = Math.round(rand);
+    var random = Math.round(rand);
     return random;
-  };
+  }
 
   function addPhrase() {
     var phrases = [
@@ -22,53 +22,52 @@ function createUsers() {
     ];
     var rand = Math.floor(Math.random() * phrases.length);
     return phrases[rand];
-  };
+  }
 
-  for (x = 0; x <= users.length; x++) {
+  for (var x = 0; x <= users.length; x++) {
     users[x] = {
       url: 'photos/' + (x + 1) + '.jpg',
       likes: getRandomInt(15, 200),
       comments: addPhrase()
     };
-  };
-};
+  }
+}
 
 createUsers();
 
 function createTemplate() {
   for (var i = 0; i <= users.length; i++) {
     var element = template.cloneNode(true);
-    element.children[0].src = users[i].url;
+    element.querySelector('img').src = users[i].url;
     element.querySelector('.picture-likes').textContent = users[i].likes;
     element.querySelector('.picture-comments').textContent = users[i].comments;
     pictures.appendChild(element);
-  };
-};
+  }
+}
 
 function showPictures() {
-  var fragment = document.createDocumentFragment();
-
-  createTemplate()
-
+  var fragment = createTemplate();
   pictures.appendChild(fragment);
-};
+}
+
+showPictures();
 
 function hideFormFraming() {
   var upload = document.querySelector('upload-overlay');
   upload.classList.add('hidden');
-};
+}
 
 hideFormFraming();
 
 function showGallery() {
   var gallery = document.querySelector('gallery-overlay');
   gallery.classList.remove('hidden');
-  galleryImage = gallery.querySelector('.gallery-overlay-image');
+  var galleryImage = gallery.querySelector('.gallery-overlay-image');
   galleryImage.src = users[0].url;
-  likesCount = gallery.querySelector('.likes-count');
+  var likesCount = gallery.querySelector('.likes-count');
   likesCount.textContent = users[0].likes;
-  commentsCount = gallery.querySelector('.comments-count');
+  var commentsCount = gallery.querySelector('.comments-count');
   commentsCount.textContent = users[0].comments;
-};
+}
 
 showGallery();
